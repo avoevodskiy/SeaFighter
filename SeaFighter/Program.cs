65bc4fb68af13945b99665fc007f84da64dc9cfe
avoevodskiy <a.voevodskiy@gmail.com>
2018-03-22 11:08:20 +0400

    Доп.пример

--- SeaFighter/Program.cs
+++ SeaFighter/Program.cs
@@ -10,12 +10,17 @@ namespace SeaFighter
     {
         static void Main(string[] args)
         {
-            int x, y;
+            string x, y;
+            //int x, y;
             Console.WriteLine("Веедите координаты удара x,y:");
-            x = Int32.Parse(Console.ReadLine());
+            x = Console.ReadLine();
+            y = Console.ReadLine();
+            /*x = Int32.Parse(Console.ReadLine());
             y = Convert.ToInt32(Console.ReadLine());
             
-            Console.WriteLine("Удар нанесен по {0},{1}",x.ToString(),y.ToString());
+            Console.WriteLine("Удар нанесен по {0},{1}",x.ToString(),y.ToString());*/
+            Console.WriteLine("Удар нанесен по {0}:{1}", x, y);
+
             Console.Read();
         }
     }
